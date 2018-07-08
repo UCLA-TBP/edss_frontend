ca80dae930afe7b1c73c00cf6e0f125203c55a8c
Carter Wu <carter.wu@ucla.edu>
2018-07-08 15:36:21 -0700

    source files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
+import { Title }     from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'UCLA Engineering Dean’s Student Showcase';
+
+  public constructor(private titleService: Title ) {
+    this.titleService.setTitle("UCLA EDSS")
+  }
 }
